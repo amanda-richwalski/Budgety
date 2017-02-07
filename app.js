a5bc9cdc72446c4318af49c6255caf69d3f40668
Amanda Richwalski <amandarichwalski@Samanthas-MacBook-Pro.local>
2017-02-07 10:15:04 -0500

    completed module 73

--- app.js
+++ app.js
@@ -24,8 +24,40 @@ var budgetController = (function() {
 			exp: 0,
 			inc: 0
 		}
-	}
+	};
 
+	return {
+		addItem: function(type, des, val) {
+			var newItem;
+
+			//ID = last ID + 1
+
+			//Create new id
+			if (data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+			} else {
+				ID = 0;
+			}
+			
+
+			//Create new item based on 'inc' or 'exp' type
+			if (type === 'exp') {
+			newItem = new Expense(ID, des, val);
+			} else if (type === 'inc') {
+				newItem = new Income(ID, des, val);
+			}
+
+			//Push it into our data structure
+			data.allItems[type].push(newItem);
+			
+			//Return the new element
+			return newItem;
+		},
+
+		testing: function() {
+			console.log(data);
+		}
+	};
 
 })();
 
@@ -74,11 +106,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
 	var ctrlAddItem = function() {
+		var input, newItem;
 
 		// 1. Get the field input data
 		var input = UICtrl.getinput();
 
 		//2. Add the item to the budget controller
+		var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
 
 		//3. Add the item to the UI
 
